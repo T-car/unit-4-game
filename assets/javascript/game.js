31d677a0747dfa86fc6f57bf2850902cc8f184f0
Tcar <thomasrcarlin@gmail.com>
2019-02-10 20:28:46 -0500

    draft of working JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,90 @@
 $( document ).ready(function(){
-    // Generates random number to guess
-    var Random=Math.floor(Math.random()*102+19)
+// random number total
+var Random=Math.floor(Math.random()*102+19)
+
+//random number for each crystal
+var num1= Math.floor(Math.random()*12+1)
+var num2= Math.floor(Math.random()*12+1)
+var num3= Math.floor(Math.random()*12+1)
+var num4= Math.floor(Math.random()*12+1)
+
+// player stats
+var playerTotal= 0; 
+var wins= 0;
+var losses = 0;
     
-    // Display random number
-    $('#magicNumber').text(Random);
-    
-    //Generate random number for each crystal
-    var num1= Math.floor(Math.random()*12+1)
-    var num2= Math.floor(Math.random()*12+1)
-    var num3= Math.floor(Math.random()*12+1)
-    var num4= Math.floor(Math.random()*12+1)
-    
-    // Variables to keep track of wins, losses and total
-    var playerTotal= 0; 
-    var wins= 0;
-    var losses = 0;
-    
+//show magic number, wins losses and player total 
+$('#wins').text(wins);
+$('#losses').text(losses);
+$('#magicNumber').text(Random);
+$('#totalScore').text(playerTotal);
   
-  $('#wins').text(wins);
-  $('#losses').text(losses);
-  
-  // Reset game
+  // choose new numbers for magic number and crystals, player score 0
   function reset(){
-        Random=Math.floor(Math.random()*102+19);
-        console.log(Random)
-        $('#magicNumber').text(Random);
-        num1= Math.floor(Math.random()*12+1);
-        num2= Math.floor(Math.random()*12+1);
-        num3= Math.floor(Math.random()*12+1);
-        num4= Math.floor(Math.random()*12+1);
-        playerTotal= 0;
-        $('#totalScore').text(playerTotal);
-        } 
-  // Display wins
+    Random=Math.floor(Math.random()*102+19);
+    console.log(Random)
+    $('#magicNumber').text(Random);
+    num1= Math.floor(Math.random()*12+1);
+    num2= Math.floor(Math.random()*12+1);
+    num3= Math.floor(Math.random()*12+1);
+    num4= Math.floor(Math.random()*12+1);
+    playerTotal= 0;
+    $('#totalScore').text(playerTotal);
+    } 
+  // add 1 to wins and reset game
   function youWon(){
     wins++; 
     $('#wins').text(wins);
     reset();
   }
-  // Display losses
+  // add 1 to losses and reset game
   function loser(){
     losses++;
     $('#losses').text(losses);
     reset()
   }
-  // Clicking crystals
+  // on click functions for crystals
     $('#crystal1').on ('click', function(){
       playerTotal = playerTotal + num1;
       console.log("New playerTotal= " + playerTotal);
       $('#totalScore').text(playerTotal); 
-            //Win & lose conditions
-          if (playerTotal == Random){
-            youWon();
+           //win if total equals magic number
+        if (playerTotal == Random){
+        youWon();
           }
-          else if ( playerTotal > Random){
-            loser();
+           //lose if total is great than magic number
+    else if ( playerTotal > Random){
+    loser();
           }   
     })  
     $('#crystal2').on ('click', function(){
       playerTotal = playerTotal + num2;
       console.log("New playerTotal= " + playerTotal);
       $('#totalScore').text(playerTotal); 
-          if (playerTotal == Random){
-            youWon();
+        if (playerTotal == Random){
+        youWon();
           }
-          else if ( playerTotal > Random){
-            loser();
+        else if ( playerTotal > Random){
+        loser();
           } 
     })  
     $('#crystal3').on ('click', function(){
       playerTotal = playerTotal + num3;
       console.log("New playerTotal= " + playerTotal);
       $('#totalScore').text(playerTotal);
-  
-            if (playerTotal == Random){
-            youWon();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
+
+        if (playerTotal == Random){
+        youWon();
+        }
+        else if ( playerTotal > Random){
+        loser();
+        } 
     })  
 
     $('#crystal4').on ('click', function(){
       playerTotal = playerTotal + num4;
       console.log("New playerTotal= " + playerTotal);
       $('#totalScore').text(playerTotal); 
-        
             if (playerTotal == Random){
             youWon();
           }
